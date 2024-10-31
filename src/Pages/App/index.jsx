d81426730631dc9117a63ebaaca24fcc3555a2db
paurrev <paulreyes391@gmail.com>
2024-10-31 01:32:02 -0500

    🚀 Add ShoppingCartProvider

--- src/Pages/App/index.jsx
+++ src/Pages/App/index.jsx
@@ -2,14 +2,17 @@ import { BrowserRouter } from 'react-router-dom';
 import './App.css';
 import AppRoutes from '../../routes/Routes';
 import Navbar from '../../Components/Navbar';
+import { ShoppingCartProvider } from '../../Context';
 
 function App() {
   return (
     <div className="min-h-screen">
-      <BrowserRouter>
-        <AppRoutes />
-        <Navbar />
-      </BrowserRouter>
+      <ShoppingCartProvider>
+        <BrowserRouter>
+          <AppRoutes />
+          <Navbar />
+        </BrowserRouter>
+      </ShoppingCartProvider>
     </div>
   );
 }
